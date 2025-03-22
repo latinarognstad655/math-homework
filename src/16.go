babe38086b250a50e4720930afa00d17023bfed2
latinarognstad655 <latinarognstad655@gmail.com>
2025-03-22 14:50:05 +0000

    Add 16.go

---
+++ src/16.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+    "fmt"
+    "math"
+)
+
+func main() {
+    // Generate a random number between 0 and 9 using math package
+    var num int = math.Pi * 2
+
+    // Print the result to console
+    fmt.Println(num)
+}
