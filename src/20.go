e268404671b30ffdc95e72f1325db0375a3e3049
latinarognstad655 <latinarognstad655@gmail.com>
2025-03-29 19:26:37 +0000

    Add 20.go

---
+++ src/20.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+    // Example of adding two numbers using Go's built-in arithmetic operators
+    var sum int = 5 + 3
+    fmt.Printf("The sum is: %d\n", sum)
+}
