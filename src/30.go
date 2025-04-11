147dea7482b17d379ce8a3fd487d32068ed2ada1
latinarognstad655 <latinarognstad655@gmail.com>
2025-04-11 11:43:19 +0000

    Add 30.go

---
+++ src/30.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+    var x float64 = 10.0
+    if x > 5.0 {
+        fmt.Println("x is greater than 5")
+    } else {
+        fmt.Println("x is not greater than 5")
+    }
+}
